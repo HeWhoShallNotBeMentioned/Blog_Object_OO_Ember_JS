88a08c7ae276f20b9ef0546a255fad9f5bcec84c
Kellen Kolbeck & Chris Underwood <student@epicodus.com>
2015-09-23 16:07:11 -0700

    add functionality for allowing usersto update & delete of posts

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -8,6 +8,16 @@ export default Ember.Component.extend({
     },
     blogPostHide: function() {
       this.set('isBlogPostShowing', false);
+    },
+
+    delete(post) {
+      if (confirm('Are you sure you want to delete this post?')) {
+        this.sendAction('destroyPost', post);
+      }
+    },
+
+    update(post, params) {
+      this.sendAction('update', post, params);
     }
   }
 });
